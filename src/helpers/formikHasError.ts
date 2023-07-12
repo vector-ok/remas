ed0b9fd938f01a4684fdb214885a60bc1546d2db
Victor Okpon <vikzy2rhyme@yahoo.com>
2023-07-12 20:51:15 +0100

    remove helper components

--- src/helpers/formikHasError.ts
+++
@@ -1,6 +0,0 @@
-import {GenericObject} from "./types";
-
-export default function formikHasError(obj: GenericObject): boolean {
-	if (!obj) return false;
-	return !!(Object.values(obj).length > 0);
-}
